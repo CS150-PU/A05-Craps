b64830382aa6936a5dfdda3a1cc88b7976e0ba4e
ryan2135 <89617523+ryan2135@users.noreply.github.com>
2024-10-21 16:57:56 +0000

    Driver update

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,79 @@
 //******************************************************************************
 // File name:   main.cpp
-// Author:      CS Faculty
-// Date:        xx/xx/xxxx (Replace xx/xx/xxxx with month/day/year)
-// Class:       CS150-xx (Replace xx with your section number)
-// Assignment:  Hello World
-// Purpose:     Display the message Hello World to the screen
-// Hours:       0.25
+// Author:      
+// Date:        
+// Class:       
+// Assignment:  06 Craps Part A
+// Purpose:     
+// Hours:       
 //******************************************************************************
 
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <ctime>
 
 using namespace std;
 
+const int HOUSE_BANK = 10000;
+
+// Craps Part A function prototypes
+void drawHeading (string title, char borderChar);
+int getPlayerBuyin (string prompt, int houseBank);
+int getPlayerBet (string prompt, int playerChips);
+void drawDieFace (int face);
+void drawDiceFace (int face1, int face2);
+int rollDie ();
+int rollDice (int& dieOne, int& dieTwo);
+
+/***********************************************************************
+Function:			main
+
+Description:	Driver for Craps game (Part A)
+
+Parameters:		None
+
+Returned:			exit status
+***********************************************************************/
+
+/******************  DO NOT CHANGE MAIN !!!!!!!!!  ********************/
 int main () {
 
-  cout << "Hello World" << endl;
-  
+  const string GAME_TITLE = "Let's Play Craps",
+    BUY_IN_PROMPT = "Enter Player Buy-In: $",
+    BET_PROMPT = "Enter Player Bet: ";
+  const char BORDER_CHARACTER = '*';
+  const unsigned int RANDOM_SEED = 4;
+
+  int houseMoney = HOUSE_BANK,
+    playerChips,
+    playerBet,
+    dieOne, dieTwo;
+
+  // srand (static_cast <unsigned> (time (static_cast <time_t*> (NULL))));
+  srand (RANDOM_SEED); // creates the same pseudo-random sequence
+  // 0 is (2, 5) on first roll, 4 is a (2, 6) on first roll
+
+  drawHeading (GAME_TITLE, BORDER_CHARACTER);
+  cout << endl;
+
+  playerChips = getPlayerBuyin (BUY_IN_PROMPT, HOUSE_BANK);
+  cout << endl;
+
+  drawHeading (GAME_TITLE, BORDER_CHARACTER);
+  cout << endl;
+
+  cout << "House Bank: $" << houseMoney + playerChips << endl;
+  cout << "Player Chips: " << playerChips << endl << endl;
+
+  playerBet = getPlayerBet (BET_PROMPT, playerChips);
+  cout << endl;
+
+  drawHeading (GAME_TITLE, BORDER_CHARACTER);
+  cout << endl;
+
+  rollDice (dieOne, dieTwo);
+  drawDiceFace (dieOne, dieTwo);
+
   return EXIT_SUCCESS;
 }
